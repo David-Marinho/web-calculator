a2262b4c670e9133841ea16d32c3d0ac6982cc69
David pequeno Marinho <63377354+David-Marinho@users.noreply.github.com>
2022-07-22 19:29:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function btn_click(numero){
+    var input = document.getElementById('input')
+    input.value += numero
+}
+
+function limpar_tudo(){
+    document.getElementById('input').value = ''
+}
+
+function operacao(simbolo){
+    var input = getElementById(input)
+    var numero1 = input.value
+    input.value = ''
+}
