56eec843404da9dc4dab88a1eac3f38fcbefe6aa
David pequeno Marinho <63377354+David-Marinho@users.noreply.github.com>
2022-07-26 19:36:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+var numero1 = 0
+var numero2 = 0
+var simbolo = ''
+var result = 0
+
+function zerar(){
+    numero1 = 0
+    numero2 = 0
+    simbolo = ''
+    result = 0
+}
+
 function btn_click(numero){
     var input = document.getElementById('input')
     input.value += numero
@@ -5,10 +17,39 @@ function btn_click(numero){
 
 function limpar_tudo(){
     document.getElementById('input').value = ''
+    zerar()
 }
 
-function operacao(simbolo){
-    var input = getElementById(input)
-    var numero1 = input.value
+function operacao(botao){
+    var input = document.getElementById('input')
+    numero1 = parseFloat(input.value, 10) 
+    simbolo = botao
     input.value = ''
+}
+
+function printar_resultado(){
+    var input = document.getElementById('input')
+    numero2 = parseFloat(input.value, 10) 
+
+    switch(simbolo){
+        case '+':
+            result = numero1 + numero2
+        break
+
+        case '-':
+            result = numero1 - numero2
+        break
+
+        case '*':
+            result = numero1 * numero2
+        break
+
+        case '/':
+            result = numero1 / numero2
+        break
+    }
+
+    input.value = result
+
+    
 }
